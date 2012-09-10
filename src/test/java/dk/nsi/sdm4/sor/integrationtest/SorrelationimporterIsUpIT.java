9af374d6422ee3e660a3635fc5e8968a367fddb4
Jakob Færch <jrf@trifork.com>
2012-09-11 00:14:59 +0200

    Api-ændring i StatusPageChecker

--- src/test/java/dk/nsi/sdm4/sor/integrationtest/SorrelationimporterIsUpIT.java
+++ src/test/java/dk/nsi/sdm4/sor/integrationtest/SorrelationimporterIsUpIT.java
@@ -6,6 +6,6 @@ import org.junit.Test;
 public class SorrelationimporterIsUpIT {
 	@Test
 	public void statusPageReturns200OK() throws Exception {
-		new StatuspageChecker().assertThatStatuspageReturns200OK("sorrelationimporter");
+		new StatuspageChecker("sorrelationimporter").assertThatStatuspageReturns200OK();
 	}
 }
